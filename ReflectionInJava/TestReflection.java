453bed30b01f8396b1223e006b72d5e816db0f62
helloAbraham <abrahamemail@yahoo.com>
2021-02-02 15:02:41 -0500

    java Reflection Project

---
+++ ReflectionInJava/TestReflection.java
@@ -0,0 +1,47 @@
+package com.Abraham.ReflectionInJava;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class TestReflection {
+
+    public static void main(String[] args) {
+        Class reflectClass = NewYork.class;
+        //Let's say we want to know the class Name
+        String className = reflectClass.getSimpleName();  //Not if we use just .getName it will print whole package "com.Abraham.ReflectionInJava.NewYork"
+        System.out.println("The class name is " + className);
+
+        //Now let's saw we want to see modifier of the class, for example public or private
+        int classModifier = reflectClass.getModifiers();
+        System.out.println(Modifier.isPublic(classModifier));
+        /**
+            Also we can check like tha, isAbstract, isFinal, isInterface, isPrivate,
+            isStatic, isStrict, isSynchronized, isVolatile
+         */
+
+            Class[] interfaces= reflectClass.getInterfaces();
+
+            Class classSuper = reflectClass.getSuperclass();
+        System.out.println("Print Super class Name " + classSuper.getSimpleName());
+
+        /**
+            READme.md
+            So you just manipulate the class that's all you are doing its nothing tricky here.
+            You can also get Object methods and return type, and parameter types and all bunch
+            of different things.
+         */
+
+        Method[] classMethods = reflectClass.getMethods();
+
+        for(Method myMethods: classMethods){
+            System.out.println("Method Name: " + myMethods.getName());
+            if(myMethods.getName().startsWith("get")){
+                System.out.println("Getter Method");
+            }else if(myMethods.getName().startsWith("set")){
+                System.out.println("setter Method");
+            }
+            System.out.println("return type: " + myMethods.getReturnType());
+        }
+    }
+
+}
---
+++ ReflectionInJava/UnitedStates.java
@@ -0,0 +1,14 @@
+package com.Abraham.ReflectionInJava;
+
+public class UnitedStates {
+
+    @Override
+    public String toString(){
+        return "God bless America.";
+    }
+
+    public int add(int a, int b){
+        System.out.println(a + b);
+        return a;
+    }
+}
