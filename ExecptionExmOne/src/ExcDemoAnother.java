9bf864ba3cadc032f16e77ecda9226f81fd27c74
helloAbraham <abrahamemail@yahoo.com>
2021-02-14 00:14:51 -0500

    exampleexception

--- ExecptionExmOne/src/ExcDemoAnother.java
+++ ExecptionExmOne/src/ExcDemoAnother.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ExcDemoAnother {
+/**
+ This is the simple program to catch the error
+ We can not divided by ZERO,
+ Also if we have two array and do something both length are not equal. It will catch the
+ out of Bounds exception.
+ */
+
+public class ExcDemoAnother {
+    public static void main(String[] args) {
+
+        int[] numerator = {3, 9, 81, 596, 81, 9, 3, 9};
+        int[] denominator = {1, 3, 0, 9, 0, 3, 1};
+
+        for(int i =0; i<numerator.length; i++){
+
+            try{
+                System.out.println(numerator[i] + " / " + denominator[i] + " is " + numerator[i] / denominator[i] );
+            }catch (ArithmeticException exc){
+                System.out.println("We can not Divided by ZERO.");
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Elements can't be reached. ");
+            }
+        }
+    }
 }
--- ExecptionExmOne/src/ExcDemoPro.java
+++ ExecptionExmOne/src/ExcDemoPro.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class ExcDemoPro {
+/**
+     This is the simple program for understanding the exception.
+     We are going to use ArrayIndexOutOfBounds
+ */
+
+public class ExcDemoPro {
+
+    public static void main(String[] args) {
+        int[] number = new int[5]; //We create an integer array with bounds, not initialize any elements
+
+
+
+        try {
+            System.out.println("Before Exception is generated.");
+            number[8] = 1001;
+            System.out.println("This won't be displayed.");
+        }catch (ArrayIndexOutOfBoundsException exc){
+            System.out.println("Hey You made a mistake...");
+        }
+
+
+    }
 }
